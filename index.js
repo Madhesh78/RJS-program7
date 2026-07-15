cd60f5d57144d1741cf9d395bf4e3105ca2f57fa
MADHESH <kulla7364@gmail.com>
2026-07-15 10:19:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-  const element= React.createElement("h1",null,"Welcome to React");
-  const root = ReactDOM.createRoot(document.getElementById("R oot"));
-  root.render(element);
+// TODO 1:
+// Create a React element using React.createElement()
+// Tag name: h1
+// Text: Welcome to React
 
+const element = React.createElement("h1",null,"Welcome to React");
+
+// TODO 2:
+// Create the React root
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// TODO 3:
+// Render the element
+
+root.render(element);
