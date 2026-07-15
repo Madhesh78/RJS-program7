3014288e76e1998b10764ddd8dfc05f50ca1a2e4
MADHESH <kulla7364@gmail.com>
2026-07-15 10:06:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
   const element= React.createElement("h1","Welcome to Recat");
-  const root = ReactDom.createRoot(document.getElementById("root"));
+  const root = ReactDom.createRoot(document.getElementById("root");
   root.render(element);
 
